f53a31c72f59cf03bc6fa98533feff875511bb7a
Seongyu <gcael@naver.com>
2026-01-27 21:25:30 +0900

    add pageable

--- src/main/kotlin/server/morningcommit/controller/ViewController.kt
+++ src/main/kotlin/server/morningcommit/controller/ViewController.kt
@@ -1,9 +1,13 @@
 package server.morningcommit.controller
 
+import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import server.morningcommit.domain.Blog
 import server.morningcommit.repository.PostRepository
 
 @Controller
@@ -12,9 +16,20 @@ class ViewController(
 ) {
 
     @GetMapping("/")
-    fun index(model: Model): String {
-        val posts = postRepository.findAll(Sort.by(Sort.Direction.DESC, "publishDate"))
+    fun index(
+        @PageableDefault(size = 9, sort = ["publishDate"], direction = Sort.Direction.DESC) pageable: Pageable,
+        @RequestParam(required = false) blog: Blog?,
+        model: Model
+    ): String {
+        val posts = if (blog != null) {
+            postRepository.findByBlog(blog, pageable)
+        } else {
+            postRepository.findAll(pageable)
+        }
+
         model.addAttribute("posts", posts)
+        model.addAttribute("blogs", Blog.entries)
+        model.addAttribute("currentBlog", blog)
         return "index"
     }
 }
