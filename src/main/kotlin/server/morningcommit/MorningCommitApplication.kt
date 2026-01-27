10d7efdcbdbfbea79d7c9e32b69016e3984ccafd
Seongyu <gcael@naver.com>
2026-01-27 15:50:48 +0900

    프로젝트 초기 설정

---
+++ src/main/kotlin/server/morningcommit/MorningCommitApplication.kt
@@ -0,0 +1,11 @@
+package server.morningcommit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MorningCommitApplication
+
+fun main(args: Array<String>) {
+    runApplication<MorningCommitApplication>(*args)
+}
---
+++ src/test/kotlin/server/morningcommit/MorningCommitApplicationTests.kt
@@ -0,0 +1,13 @@
+package server.morningcommit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MorningCommitApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
