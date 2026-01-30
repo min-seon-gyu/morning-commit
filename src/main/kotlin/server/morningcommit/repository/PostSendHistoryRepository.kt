bdd6f6c3299cf26cbeecb4faddd103d044e79841
Seongyu <gcael@naver.com>
2026-01-30 20:05:02 +0900

    refactor: 리포지토리 인터페이스 리팩터링 및 빌드 설정 업데이트

--- src/main/kotlin/server/morningcommit/repository/PostSendHistoryRepository.kt
+++ src/main/kotlin/server/morningcommit/repository/PostSendHistoryRepository.kt
@@ -1,12 +1,6 @@
 package server.morningcommit.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Modifying
-import org.springframework.data.jpa.repository.Query
 import server.morningcommit.domain.PostSendHistory
 
-interface PostSendHistoryRepository : JpaRepository<PostSendHistory, Long> {
-    @Modifying
-    @Query("DELETE FROM PostSendHistory h WHERE h.subscriberId = :subscriberId")
-    fun deleteBySubscriberId(subscriberId: Long)
-}
+interface PostSendHistoryRepository : JpaRepository<PostSendHistory, Long>
