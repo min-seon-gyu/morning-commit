b8f7fc011624d87d8d2ef4534b4a8c7c3d94995a
Seongyu <gcael@naver.com>
2026-02-08 03:31:32 +0900

    refactor PostService to return RestPage and add RestPage class for pagination

--- src/main/kotlin/server/morningcommit/service/PostService.kt
+++ src/main/kotlin/server/morningcommit/service/PostService.kt
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import server.morningcommit.config.RedisConfig
+import server.morningcommit.config.RestPage
 import server.morningcommit.domain.Blog
 import server.morningcommit.domain.Post
 import server.morningcommit.repository.PostRepository
@@ -20,16 +21,18 @@ class PostService(
         cacheNames = [RedisConfig.POST_LISTING],
         key = "'page:' + #pageable.pageNumber + ':size:' + #pageable.pageSize + ':sort:' + #pageable.sort.toString()"
     )
-    fun findAll(pageable: Pageable): Page<Post> {
-        return postRepository.findAll(pageable)
+    fun findAll(pageable: Pageable): RestPage<Post> {
+        val page = postRepository.findAll(pageable)
+        return RestPage(page.content, page.number, page.size, page.totalElements)
     }
 
     @Cacheable(
         cacheNames = [RedisConfig.POST_LISTING],
         key = "'blog:' + #blog.name() + ':page:' + #pageable.pageNumber + ':size:' + #pageable.pageSize + ':sort:' + #pageable.sort.toString()"
     )
-    fun findByBlog(blog: Blog, pageable: Pageable): Page<Post> {
-        return postRepository.findByBlog(blog, pageable)
+    fun findByBlog(blog: Blog, pageable: Pageable): RestPage<Post> {
+        val page = postRepository.findByBlog(blog, pageable)
+        return RestPage(page.content, page.number, page.size, page.totalElements)
     }
 
     fun findAllIds(): List<Long> {
