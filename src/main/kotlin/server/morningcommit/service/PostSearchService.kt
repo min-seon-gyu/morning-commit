3d75b7a8395efc80d6b4011e37e5329d62778ea0
Seongyu <gcael@naver.com>
2026-02-10 21:27:53 +0900

    Revert "feat: 검색 문서 재색인 및 삭제를 위한 관리자 엔드포인트 추가"
    This reverts commit 8a991bb3e108ef9994b23f0dc2a22fef165acc93.

--- src/main/kotlin/server/morningcommit/service/PostSearchService.kt
+++ src/main/kotlin/server/morningcommit/service/PostSearchService.kt
@@ -7,13 +7,11 @@ import org.springframework.stereotype.Service
 import server.morningcommit.domain.Blog
 import server.morningcommit.domain.Post
 import server.morningcommit.domain.PostDocument
-import server.morningcommit.repository.PostRepository
 import server.morningcommit.repository.PostSearchRepository
 
 @Service
 class PostSearchService(
-    private val postSearchRepository: PostSearchRepository,
-    private val postRepository: PostRepository
+    private val postSearchRepository: PostSearchRepository
 ) {
     private val log = LoggerFactory.getLogger(javaClass)
 
@@ -32,24 +30,5 @@ class PostSearchService(
         val documents = posts.map { PostDocument.from(it) }
 
         postSearchRepository.saveAll(documents)
-    }
-
-    fun reindexAll(): Long {
-        val allPosts = postRepository.findAll()
-        if (allPosts.isEmpty()) return 0
-
-        val documents = allPosts.map { PostDocument.from(it) }
-        postSearchRepository.saveAll(documents)
-
-        log.info("Reindexed {} posts to Elasticsearch", documents.size)
-        return documents.size.toLong()
-    }
-
-    fun deleteAll(): Long {
-        val count = postSearchRepository.count()
-        postSearchRepository.deleteAll()
-
-        log.info("Deleted {} documents from Elasticsearch", count)
-        return count
     }
 }
